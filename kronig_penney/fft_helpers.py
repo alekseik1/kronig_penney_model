d7ee92a1377740e41c2f2f27a60f6985512cb35a
alekseik1 <1alekseik1@gmail.com>
2021-01-08 21:08:02 +0300

    feat: cache FFT coefficients

--- kronig_penney/fft_helpers.py
+++ kronig_penney/fft_helpers.py
@@ -2,6 +2,8 @@ import numpy as np
 
 
 class FourierTransformer:
+    _freq_coeffs_cache: np.ndarray = None
+
     def add_grid(self, period: float, n_dots: int) -> 'FourierTransformer':
         """
         Creates direct grid
@@ -23,7 +25,9 @@ class FourierTransformer:
 
     @property
     def freq_coeffs(self) -> np.ndarray:
-        return np.fft.rfft(self.potential_function_direct(self.x_grid)) * self._delta
+        if self._freq_coeffs_cache is None:
+            self._freq_coeffs_cache = np.fft.rfft(self.potential_function_direct(self.x_grid)) * self._delta
+        return self._freq_coeffs_cache
 
     def convert_back(self, values: np.ndarray) -> np.ndarray:
         return np.fft.irfft(values / self._delta, self.x_grid.size)
