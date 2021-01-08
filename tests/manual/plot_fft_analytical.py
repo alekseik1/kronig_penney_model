e84ebd205298d09b10bb0a3980072caff0b2469a
alekseik1 <1alekseik1@gmail.com>
2021-01-08 18:36:19 +0300

    feat(manual tests): more graphs

--- tests/manual/plot_fft_analytical.py
+++ tests/manual/plot_fft_analytical.py
@@ -20,26 +20,37 @@ def pot_direct(x):
 
 if __name__ == '__main__':
     tr = FourierTransformer()
-    fig, axes = plt.subplots(3, 1, figsize=(9, 9), dpi=300)    # type: plt.Figure, List[plt.Axes]
+    fig, axes = plt.subplots(4, 1, figsize=(9, 16), dpi=300)    # type: plt.Figure, List[plt.Axes]
 
     tr.\
-        add_grid(b/2, 10**3).\
+        add_grid(b/2, 10**5).\
         add_potential(pot_direct)
 
     freq = tr.freq_grid
-    coeffs = tr.freq_coeffs
+    coeffs = np.real(tr.freq_coeffs)
     anal = analytical_ft(tr.freq_grid)
-    # Direct space
-    axes[2].plot(tr.x_grid, pot_direct(tr.x_grid))
     # Fourier
     for ax in axes[:2]:
         ax.plot(freq, coeffs, label='numeric', alpha=0.3)
         ax.scatter(freq, anal, label='analytical', alpha=0.3, s=2)
         ax.legend()
         ax.grid()
-    axes[0].set_xlim(0, 10)
-    axes[0].set_ylim(-10, 5)
-    #axes[0].set_ylim(-0.1, 0.1)
+    #axes[0].set_xlim(0.8*np.max(tr.freq_grid), np.max(tr.freq_grid))
+    #axes[0].set_ylim(-0.5, 0.5)
+    axes[0].set_xlim(0, 20.)
+    axes[0].set_ylim(-10, 5.)
+    axes[0].set_title('Near the border')
+    axes[1].set_title('Fourier images')
+    # Difference
+    #axes[2].scatter(freq, np.abs((coeffs - anal) / anal), label='relative difference', alpha=0.3, s=2)
+    axes[2].scatter(freq, np.abs((coeffs - anal) / anal), label='relative difference')
+    axes[2].set_xlim(0, 100)
+    axes[2].legend()
+    axes[2].grid()
+    # Direct space
+    axes[-1].set_title('Potential in direct space')
+    axes[-1].plot(tr.x_grid, pot_direct(tr.x_grid))
+    axes[-1].grid()
 
     fig.show()
     plt.close(fig)
